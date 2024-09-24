582dab19491ba2340ec070d5e08cf136607e7e9f
kaikai8812 <kai.aoyama@dely.jp>
2024-09-24 14:12:49 +0900

    ⑨保存した画像を、取得する

--- LockedCameraCaptureSample/ContentView.swift
+++ LockedCameraCaptureSample/ContentView.swift
@@ -23,6 +23,19 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    private func getLockedCaptureImage() {
+        for url in LockedCameraCaptureManager.shared.sessionContentURLs {
+            let fileUrl = url.appendingPathComponent("LockedCapture")
+            do {
+                let data = try Data(contentsOf: fileUrl)
+                guard let uiImage = UIImage(data: data) else { return }
+                imageValues.append(uiImage)
+            } catch {
+                // error handling
+            }
+        }
+    }
 }
 
 #Preview {
